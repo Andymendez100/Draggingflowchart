555162cde0932b625c336bc87ecaa5abc541a2f9
Andy <andymendez100@gmail.com>
2022-05-04 09:34:25 -0700

    finished with new design for grid

--- src/components/Grid/DraggableCard.tsx
+++ src/components/Grid/DraggableCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
-import Xarrow, { useXarrow, Xwrapper } from 'react-xarrows';
+import { useXarrow } from 'react-xarrows';
 
 declare module 'react-draggable' {
   export interface DraggableProps {
@@ -13,46 +13,32 @@ const DraggableCard: React.FC<{
   id: string;
   handleConnection: Function;
 }> = ({ cardData, id, handleConnection }) => {
-  const nodeRef = React.useRef(null);
+  const nodeRef = React.useRef<HTMLDivElement>(null);
 
   const updateXarrow = useXarrow();
 
-  const handleOnStop = (event: any) => {
-    updateXarrow();
-    if (event.target.classList.contains('card')) {
-      let card = event.target.children;
-      const g = document.getElementById('grid');
-      const gData = g?.getBoundingClientRect();
-      const insideGridCheck =
-        event.y > (gData?.top || -1) &&
-        event.y < (gData?.bottom || -1) &&
-        event.x > (gData?.left || -1) &&
-        event.x < (gData?.right || -1);
 
-      if (insideGridCheck) {
-        card[0]?.classList.remove('hidden');
-      }
-
-      if (!insideGridCheck && !card[0]?.classList.contains('hidden')) {
-        card[0]?.classList.add('hidden');
-      }
-    }
-  };
 
   return (
     <Draggable
-      onStop={(event) => handleOnStop(event)}
-      grid={[90, 100]}
+      onStop={updateXarrow}
+      grid={[85, 84]}
       nodeRef={nodeRef}
       onDrag={updateXarrow}
     >
-      <div className='card' ref={nodeRef} id={id} data-id={cardData.id} data-allowed={cardData.allowed}>
+      <div
+        className='card'
+        ref={nodeRef}
+        id={id}
+        data-id={cardData.id}
+        data-allowed={cardData.allowed}
+      >
         <img
-          className='hidden'
+          // className='hidden'
           src='./link.png'
           alt=''
           onClick={(event) => {
-            handleConnection(event)
+            handleConnection(event);
           }}
         />
         {cardData.title}
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Xarrow, { Xwrapper } from 'react-xarrows';
+import Xarrow, { anchorType, Xwrapper } from 'react-xarrows';
 // Allowed means cards it can connect to
 
 import GridCards from './gridData.json';
@@ -7,22 +7,96 @@ import DraggableCard from './DraggableCard';
 import './Grid.css';
 
 const Grid: React.FC = () => {
+  const rowSwimLanes = [
+    'Telescope Assy Sell-Off',
+    'Telescope Assy Procurement',
+    'Telescope Assy I&T',
+    'Telescope Receivers',
+    'Telescope Assy Design',
+  ];
   const [currentArrow, setCurrentArrow] = useState({
     start: '',
     end: '',
     allowed: '',
+    endAnchor: '',
+    startAnchor: '',
   });
   const [arrows, setArrows] = useState<
-    { start: string; end: string; allowed: string; color?: string }[]
+    {
+      start: string;
+      end: string;
+      allowed: string;
+      color?: string;
+      endAnchor: string;
+      startAnchor: string;
+    }[]
   >([]);
-  let gridHeight = 5;
+  let gridHeight = 7;
   let gridWidth = 14;
 
   const buildGrid = (width: number, height: number): any => {
-    return Array.from(Array(height)).map((row: any, index: number) => {
+    return Array.from(Array(height)).map((row: any, rowindex: number) => {
+      if (rowindex < 1) {
+        return (
+          <tr data-row={rowindex + 1} key={rowindex} className='draggable-row'>
+            {GridCards.data.map((card, cardindex) => {
+              if (cardindex < 12) {
+                return (
+                  <td
+                    data-column={cardindex}
+                    key={cardindex}
+                    // id='swim-lane-box'
+                    className='draggableBox'
+                  >
+                    <DraggableCard
+                      id={`draggableCard${card.id}`}
+                      cardData={card}
+                      key={cardindex}
+                      handleConnection={handleConnection}
+                    />
+                  </td>
+                );
+              }
+              return null;
+            })}
+          </tr>
+        );
+      } else if (rowindex === 1) {
+        return (
+          <tr data-row={rowindex + 1} key={rowindex} className='draggable-row'>
+            {GridCards.data.map((card, cardindex) => {
+              if (cardindex > 11) {
+                return (
+                  <td
+                    data-column={cardindex}
+                    key={cardindex}
+                    className='draggableBox'
+                    // id='swim-lane-box'
+                  >
+                    <DraggableCard
+                      id={`draggableCard${card.id}`}
+                      cardData={card}
+                      key={cardindex}
+                      handleConnection={handleConnection}
+                    />
+                  </td>
+                );
+              }
+              return null;
+            })}
+          </tr>
+        );
+      }
       return (
-        <tr data-row={index + 1} key={index}>
+        <tr data-row={rowindex + 1} key={rowindex}>
           {Array.from(Array(width)).map((column: any, index: number) => {
+            if (index === 0 && rowindex > 1) {
+              return (
+                <td data-column={index} key={index} id='swim-lane-box'>
+                  <span id='swim-lanes'>{rowSwimLanes[rowindex - 2]}</span>
+                </td>
+              );
+            }
             return <td data-column={index + 1} key={index}></td>;
           })}
         </tr>
@@ -32,6 +106,9 @@ const Grid: React.FC = () => {
 
   const handleConnection = (event: any) => {
     const currentTarget = event.target.parentNode.id;
+    const indexData = currentTarget.split('draggableCard')[1] - 1;
+    console.log('currentTarget', indexData);
+    console.log('GridCards', GridCards.data[indexData].startAnchor);
     const remove: number[] = [];
     const disconnect = arrows.filter((arrow, index) => {
       const condition =
@@ -50,27 +127,48 @@ const Grid: React.FC = () => {
       currentArrow.start !== currentTarget &&
       disconnect.length === 0
     ) {
+      console.log('endAnchor', GridCards.data[indexData]);
       setArrows(
         arrows.concat([
           {
+            ...currentArrow,
             start: currentArrow.start,
             end: currentTarget,
             allowed: currentArrow.allowed,
+
+            endAnchor: GridCards.data[indexData].endAnchor,
           },
         ])
       );
-      setCurrentArrow({ start: '', end: '', allowed: '' });
+      setCurrentArrow({
+        start: '',
+        end: '',
+        allowed: '',
+        endAnchor: '',
+        startAnchor: '',
+      });
     }
     // check if this exists in state
     else if (disconnect.length !== 0) {
-      // WIP
       setArrows(arrows.filter((arrow, index) => !remove.includes(index)));
-      setCurrentArrow({ start: '', end: '', allowed: '' });
+      setCurrentArrow({
+        start: '',
+        end: '',
+        allowed: '',
+        endAnchor: '',
+        startAnchor: '',
+      });
     } else {
+      console.log(
+        'test',
+        GridCards.data[parseInt(currentTarget.split('draggableCard')[1] + 1)]
+      );
       setCurrentArrow({
         ...currentArrow,
         start: currentTarget,
         allowed: event.target.parentNode.dataset.allowed,
+        // endAnchor: '',
+        startAnchor: GridCards.data[indexData].startAnchor,
       });
     }
   };
@@ -88,62 +186,39 @@ const Grid: React.FC = () => {
     );
   };
 
-  const buildCardRows = (data: any) => {
-    const rowLength = 11;
-    let row: any[] = [];
-    let rows: any = [];
-
-    // build the row up to rowLength
-    GridCards.data.forEach((card: any, index: any) => {
-      if (row.length < rowLength) {
-        row.push(
-          <DraggableCard
-            id={`draggableCard${card.id}`}
-            cardData={card}
-            key={index}
-            handleConnection={handleConnection}
-          />
-        );
-      }
-
-      if (row.length === rowLength || index + 1 === GridCards.data.length) {
-        rows.push(
-          <div className='card-row' key={rows.length}>
-            {row}
-          </div>
-        );
-        row = [];
-      }
-    });
-
-    return rows;
-  };
-
-  const renderCardRows = (rows: any) => {
-    return rows.map((card: any) => card);
-  };
-
   return (
-    <div>
+    <>
       <Xwrapper>
         {/* render arrows here */}
-        {arrows.map((arrow) => (
+        {arrows.map((arrow, index) => (
           <Xarrow
+            key={index}
             {...arrow}
             lineColor={arrow.color ? arrow.color : '#3568c1'}
             headColor={arrow.color ? arrow.color : '#3568c1'}
-            path='smooth'
-            // gridBreak="110px"
+            path='grid'
+            endAnchor={arrow.endAnchor as anchorType}
+            startAnchor={arrow.startAnchor as anchorType}
+            // gridBreak='50px'
+            // _cpx1Offset={-150}
+            // _cpy1Offset={-36}
+            // _cpx2Offset={150}
+            // _cpy2Offset={-38}
             strokeWidth={2}
           />
         ))}
-        <div id='card-container'>
-          {renderCardRows(buildCardRows(GridCards))}
-        </div>
+        {/* <div id='grid-heading'>Telescope Assembly Network Diagram</div> */}
+        {/* <div id='card-container'>{renderCardRows(buildCardRows())}</div> */}
         {/* 5x14 */}
         <div id='grid-container'>
           <table id='grid'>
-            <tbody>{buildGrid(gridWidth, gridHeight)}</tbody>
+            <span id='swim-lane-background'></span>
+            <caption id='grid-heading'>
+              Telescope Assembly Network Diagram
+            </caption>
+            <tbody className='tablebody'>
+              {buildGrid(gridWidth, gridHeight)}
+            </tbody>
           </table>
         </div>
       </Xwrapper>
@@ -155,7 +230,7 @@ const Grid: React.FC = () => {
       >
         SUBMIT
       </button>
-    </div>
+    </>
   );
 };
 
